61aac5e61fd0d5cf09d6fea239204ce80f1431de
João Victor <95556375+HomemJaguar111@users.noreply.github.com>
2024-03-03 02:59:40 -0300

    !

--- crop.py
+++ crop.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import folium
-from folium.plugins import Draw
-from streamlit_folium import folium_static
+#from folium.plugins import Draw
+#from streamlit_folium import folium_static
 import pandas as pd
 
 
@@ -37,7 +37,8 @@ def main():
         folium.Marker([la,lo], tooltip=c).add_to(m)       
     
     st.title("Mapa dos CropCircles")
-    folium_static(m)          
+    return m
+    #folium_static(m)          
     
 if __name__ == "__main__":
     main()
