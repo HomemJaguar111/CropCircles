738b65919109406ee1bcae60b12e8edc3523ca93
João Victor <95556375+HomemJaguar111@users.noreply.github.com>
2024-03-06 05:42:58 -0300

    Gráficos de Barra e Pizza adicionados

--- crop.py
+++ crop.py
@@ -3,7 +3,13 @@ import folium
 from folium.plugins import Draw
 from folium.plugins import HeatMap
 from streamlit_folium import folium_static
+
+
+import plotly.express as px
+
 import pandas as pd
+
+st.set_page_config(layout='wide')
 
 def main():
     #Mapa Definido  ###############################################
@@ -32,7 +38,7 @@ def main():
     
     #Adicionando função de calor no mapa
     coordTotal = cropData[["Latitude","Longitude"]].values.tolist()
-    HeatMap(coordTotal, radius=50).add_to(m)
+    HeatMap(coordTotal, radius=52).add_to(m)
       
     ########################################################################
        
@@ -45,6 +51,31 @@ def main():
     ###################################################################
     
     #####################################################################
+    #Gráficos
+        
+    #Gráfico em Pizza dos Paízes
+    cropCountry = cropData['Country'].value_counts()
+    pieCountryCrops = px.pie(cropCountry, 
+                  values=cropCountry.values, 
+                  names=cropCountry.index, 
+                  title='Crop Circles por país')
+    
+    # Gráfico em Barra - Crops por Ano
+    
+    
+    yearCrops = cropData[["Year","Country"]].value_counts().reset_index()
+    #yearCropsA = yearCrops.sort_values(by='Year',ascending=False )
+    #yearCropsA
+    yearCrops
+    yearCropsBar = px.bar(yearCrops, 
+                       x="Year",
+                       y="count",
+                       color="Country",
+                       title="Crops em cada ano")
+    
+    #########################################################################
+   
+    #############################################################
     #Chamadas no Site
     
     #Título do site
@@ -53,6 +84,10 @@ def main():
     #Mapa Principal
     #return m
     folium_static(m)  
+    #Gráfico em Pizza de Crops por País
+    pieCountryCrops
+    #Gráfico de Barras - Crops por Ano        
+    yearCropsBar
     #dataseet
     cropData        
     ##################################################################
